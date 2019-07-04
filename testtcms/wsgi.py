22dd4b745dae0dc33a3e25991f1fe4a97f20d732
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-04 23:41:14 +0100

    Change module name Change the module TestTcms to testtcms.

--- testtcms/wsgi.py
+++ testtcms/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TestTcms project.
+WSGI config for testtcms project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestTcms.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testtcms.settings')
 
 application = get_wsgi_application()
