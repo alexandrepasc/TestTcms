80273cd64903f72f1c17a8223aee2070701adf75
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-07 00:11:03 +0100

    Test mgr Set view name as python convention.

--- mgrtests/views.py
+++ mgrtests/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-
     setattr(request, 'view', 'home')
 
     return render(request, 'home.html')
 
 
-def testMgr(request):
-
+def test_mgr(request):
     setattr(request, 'view', 'testMgr')
 
     return render(request, 'testMgr.html')
