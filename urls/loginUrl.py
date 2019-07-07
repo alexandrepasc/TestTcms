525912bf1fd0762c61d2ae218ab26a6814d37c83
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-07 11:29:47 +0100

    Urls Include loginUrl to urls. Correct path to template.

--- urls/loginUrl.py
+++ urls/loginUrl.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^login$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
