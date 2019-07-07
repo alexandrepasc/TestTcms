bbe62cc063a35878076f93a76b6158c3187940e9
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-07 11:22:17 +0100

    Login Add the login urls.

---
+++ urls/loginUrl.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    url(r'^login$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+]
