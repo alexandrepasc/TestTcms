9d50adefafe129926223e9f65fe108eb5124b6c8
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-13 15:13:07 +0100

    Models Remove unique option from name fields on all models.

--- mgrtests/models.py
+++ mgrtests/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
 
 class Version(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     product = models.ForeignKey(Product, related_name='versions', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='versions', on_delete=models.CASCADE)
@@ -20,7 +20,7 @@ class Version(models.Model):
 
 class TestSuite(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     product = models.ForeignKey(Product, related_name='suites', on_delete=models.CASCADE)
     version = models.ForeignKey(Version, related_name='suites', on_delete=models.CASCADE)
@@ -32,7 +32,7 @@ class TestSuite(models.Model):
 
 class TestCase(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     suite = models.ForeignKey(TestSuite, related_name='cases', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='cases', on_delete=models.CASCADE)
@@ -40,7 +40,7 @@ class TestCase(models.Model):
 
 class TestRuns(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     cases = models.TextField()
     created_by = models.ForeignKey(User, related_name='runs', on_delete=models.CASCADE)
