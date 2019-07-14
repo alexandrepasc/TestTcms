f0e0e89282eeab2d34805cc4a9af103ecad460fb
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-14 23:23:30 +0100

    New tag Add forms folder with tagForms. Add new tag form. Add new tag urls and view. Add title to base template and request attribute to implement it.

--- urls/tagUrl.py
+++ urls/tagUrl.py
@@ -5,4 +5,6 @@ from views import tagViews
 urlpatterns = [
     url(r'^tag', tagViews.tag, name='tag'),
     url(r'^tag/$', tagViews.tag, name='tag'),
+    url(r'^newTag/$', tagViews.new_tag, name='newTag'),
+    url(r'^newTag/?(\S)$', tagViews.new_tag, name='newTag'),
 ]
--- views/tagViews.py
+++ views/tagViews.py
@@ -2,12 +2,34 @@ import uuid
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
+from forms.tagForms import NewForm
 from mgrtests.models import Tag
 
 
+@login_required
 def tag(request):
     items = Tag.objects.all().order_by('name')
 
     setattr(request, 'view', 'tag')
+    setattr(request, 'title', 'Tags')
 
-    return render(request, 'others/tag.html')
+    return render(request, 'others/tag.html', {'items': items})
+
+
+@login_required
+def new_tag(request):
+    if request.method == 'POST':
+        form = NewForm(request.POST)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.id = uuid.uuid4()
+            item.created_by = request.user
+            item.save()
+
+            return redirect('/newTag/?page=reload')
+
+    else:
+        form = NewForm()
+
+    return render(request, 'include/newItem.html', {'form': form})
