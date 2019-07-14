3f4e1d776a1ce5aee52fac56c19142ff520b91e9
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-14 13:28:39 +0100

    1st detail draft Add template and urls.

--- urls/versionUrl.py
+++ urls/versionUrl.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^version/$', versionViews.version, name='version'),
     url(r'^newVersion/$', versionViews.new_version, name='newVersion'),
     url(r'^newVersion/?(\S)$', versionViews.new_version, name='newVersion'),
+    url(r'^detailVersion/(?P<id>[0-9a-f-]+)$', versionViews.detail_version, name='detailVersion'),
+    url(r'^detailVersion/(?P<id>[0-9a-f-]+)/?(\S)$', versionViews.detail_version, name='detailVersion'),
 ]
