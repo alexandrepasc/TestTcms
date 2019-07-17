f1cd809d60881c46cd8785c08870c0423ef8fbfe
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-17 23:39:49 +0100

    Models
    Change models.
    Create initial migrations.

--- mgrtests/models.py
+++ mgrtests/models.py
@@ -8,6 +8,9 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_products', on_delete=models.CASCADE, null=True)
+    updated_at = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.name
@@ -19,6 +22,9 @@ class Version(models.Model):
     description = models.CharField(max_length=100)
     product = models.ForeignKey(Product, related_name='versions', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='versions', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_versions', on_delete=models.CASCADE, null=True)
+    updated_at = models.DateTimeField(null=True)
 
 
 class TestSuite(models.Model):
@@ -28,6 +34,9 @@ class TestSuite(models.Model):
     product = models.ForeignKey(Product, related_name='suites', on_delete=models.CASCADE)
     version = models.ForeignKey(Version, related_name='suites', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='suites', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_suites', on_delete=models.CASCADE, null=True)
+    updated_at = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.name
@@ -39,6 +48,9 @@ class TestCase(models.Model):
     description = models.CharField(max_length=100)
     suite = models.ForeignKey(TestSuite, related_name='cases', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='cases', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_cases', on_delete=models.CASCADE, null=True)
+    updated_at = models.DateTimeField(null=True)
 
 
 class TestRuns(models.Model):
@@ -47,6 +59,9 @@ class TestRuns(models.Model):
     description = models.CharField(max_length=100)
     cases = models.TextField()
     created_by = models.ForeignKey(User, related_name='runs', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_runs', on_delete=models.CASCADE, null=True)
+    updated_at = models.DateTimeField(null=True)
 
 
 class Tag(models.Model):
@@ -55,6 +70,7 @@ class Tag(models.Model):
     description = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, related_name='tags', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_tags', on_delete=models.CASCADE, null=True)
     updated_at = models.DateTimeField(null=True)
 
 
@@ -64,4 +80,5 @@ class Component(models.Model):
     description = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, related_name='components', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, related_name='update_components', on_delete=models.CASCADE, null=True)
     updated_at = models.DateTimeField(null=True)
