8052396e64d2f114b99abcf1b00699db9afb77df
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-20 12:25:50 +0100

    Convert date
    Change all the datetime to milliseconds

--- mgrtests/models.py
+++ mgrtests/models.py
@@ -8,9 +8,11 @@ class Product(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, related_name='products', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_products', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
 
     def __str__(self):
         return self.name
@@ -22,9 +24,11 @@ class Version(models.Model):
     description = models.CharField(max_length=100)
     product = models.ForeignKey(Product, related_name='versions', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='versions', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_versions', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
 
 
 class TestSuite(models.Model):
@@ -34,9 +38,11 @@ class TestSuite(models.Model):
     product = models.ForeignKey(Product, related_name='suites', on_delete=models.CASCADE)
     version = models.ForeignKey(Version, related_name='suites', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='suites', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_suites', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
 
     def __str__(self):
         return self.name
@@ -48,9 +54,11 @@ class TestCase(models.Model):
     description = models.CharField(max_length=100)
     suite = models.ForeignKey(TestSuite, related_name='cases', on_delete=models.CASCADE)
     created_by = models.ForeignKey(User, related_name='cases', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_cases', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
 
 
 class TestRuns(models.Model):
@@ -59,9 +67,11 @@ class TestRuns(models.Model):
     description = models.CharField(max_length=100)
     cases = models.TextField()
     created_by = models.ForeignKey(User, related_name='runs', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_runs', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
 
 
 class Tag(models.Model):
@@ -69,9 +79,11 @@ class Tag(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, related_name='tags', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_tags', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
 
 
 class Component(models.Model):
@@ -79,6 +91,8 @@ class Component(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, related_name='components', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_components', on_delete=models.CASCADE, null=True)
-    updated_at = models.DateTimeField(null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
