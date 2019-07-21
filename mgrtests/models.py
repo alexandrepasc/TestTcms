62553556f79b6ee24f458c412c610e0628a1f62a
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-21 23:11:33 +0100

    Models
    Add new table to associate suites with cases.
    Add new fields to TestSuites.
    Some corrections.

--- mgrtests/models.py
+++ mgrtests/models.py
@@ -31,49 +31,6 @@ class Version(models.Model):
     updated_at = models.BigIntegerField(null=True)
 
 
-class TestSuite(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
-    product = models.ForeignKey(Product, related_name='suites', on_delete=models.CASCADE)
-    version = models.ForeignKey(Version, related_name='suites', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, related_name='suites', on_delete=models.CASCADE)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.BigIntegerField()
-    updated_by = models.ForeignKey(User, related_name='update_suites', on_delete=models.CASCADE, null=True)
-    # updated_at = models.DateTimeField(null=True)
-    updated_at = models.BigIntegerField(null=True)
-
-    def __str__(self):
-        return self.name
-
-
-class TestCase(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
-    suite = models.ForeignKey(TestSuite, related_name='cases', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, related_name='cases', on_delete=models.CASCADE)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.BigIntegerField()
-    updated_by = models.ForeignKey(User, related_name='update_cases', on_delete=models.CASCADE, null=True)
-    # updated_at = models.DateTimeField(null=True)
-    updated_at = models.BigIntegerField(null=True)
-
-
-class TestRuns(models.Model):
-    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
-    cases = models.TextField()
-    created_by = models.ForeignKey(User, related_name='runs', on_delete=models.CASCADE)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    created_at = models.BigIntegerField()
-    updated_by = models.ForeignKey(User, related_name='update_runs', on_delete=models.CASCADE, null=True)
-    # updated_at = models.DateTimeField(null=True)
-    updated_at = models.BigIntegerField(null=True)
-
-
 class Tag(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
     name = models.CharField(max_length=30)
@@ -94,5 +51,56 @@ class Component(models.Model):
     # created_at = models.DateTimeField(auto_now_add=True)
     created_at = models.BigIntegerField()
     updated_by = models.ForeignKey(User, related_name='update_components', on_delete=models.CASCADE, null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
+
+
+class TestSuite(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    product = models.ForeignKey(Product, related_name='suites', on_delete=models.CASCADE, null=True)
+    version = models.ForeignKey(Version, related_name='suites', on_delete=models.CASCADE, null=True)
+    component = models.ForeignKey(Component, related_name='suites', on_delete=models.CASCADE, null=True)
+    tag = models.ForeignKey(Tag, related_name='suites', on_delete=models.CASCADE, null=True)
+    created_by = models.ForeignKey(User, related_name='suites', on_delete=models.CASCADE)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
+    updated_by = models.ForeignKey(User, related_name='update_suites', on_delete=models.CASCADE, null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TestCase(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    # suite = models.ForeignKey(TestSuite, related_name='cases', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='cases', on_delete=models.CASCADE)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
+    updated_by = models.ForeignKey(User, related_name='update_cases', on_delete=models.CASCADE, null=True)
+    # updated_at = models.DateTimeField(null=True)
+    updated_at = models.BigIntegerField(null=True)
+
+
+class TestSuitesCases(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
+    suite = models.ForeignKey(TestSuite, related_name='suite_cases', on_delete=models.CASCADE)
+    case = models.ForeignKey(TestCase, related_name='case_suites', on_delete=models.CASCADE)
+
+
+class TestRuns(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4())
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    cases = models.TextField()
+    created_by = models.ForeignKey(User, related_name='runs', on_delete=models.CASCADE)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.BigIntegerField()
+    updated_by = models.ForeignKey(User, related_name='update_runs', on_delete=models.CASCADE, null=True)
     # updated_at = models.DateTimeField(null=True)
     updated_at = models.BigIntegerField(null=True)
