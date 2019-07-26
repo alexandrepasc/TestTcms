e2fd1222c4d18f702657fa16457f7ae3bbd6584e
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-26 21:26:21 +0100

    Edit url

--- urls/suiteUrl.py
+++ urls/suiteUrl.py
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^newSuite/$', suiteViews.new_suite, name='newSuite'),
     url(r'^detailSuite/(?P<pk>[0-9a-f-]+)$', suiteViews.detail_suite, name='detailSuite'),
     url(r'^detailSuite/(?P<pk>[0-9a-f-]+)/$', suiteViews.detail_suite, name='detailSuite'),
+    url(r'^editSuite/(?P<pk>[0-9a-f-]+)$', suiteViews.edit_suite, name='editSuite'),
+    url(r'^editSuite/(?P<pk>[0-9a-f-]+)/$', suiteViews.edit_suite, name='editSuite'),
 ]
