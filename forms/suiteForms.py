d3e1ad22955e8d7ed3362021e25efca1c24fa603
alexandrepasc <alexandre.pascoal@gmail.com>
2019-07-26 20:26:45 +0100

    Edit suite
    Add draft for edit form.

--- forms/suiteForms.py
+++ forms/suiteForms.py
@@ -46,7 +46,6 @@ class NewForm(forms.ModelForm):
 
 
 class DetailForm(forms.ModelForm):
-    # name = forms.Textarea(attrs={'readonly': True})
     name = forms.CharField(
         widget=forms.Textarea(attrs={'rows': 1, 'readonly': True}),
         max_length=30
@@ -73,3 +72,29 @@ class DetailForm(forms.ModelForm):
         model = TestSuite
         fields = ['name', 'description', 'product', 'version', 'component', 'tag',
                   'created_by', 'created_at', 'updated_by', 'updated_at']
+
+
+class EditForm(forms.ModelForm):
+    name = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 1}),
+        max_length=30
+    )
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 5, 'placeholder': 'Suite description'}),
+        help_text='The max length of the text is 100.',
+        max_length=100,
+    )
+
+    class Meta:
+        model = TestSuite
+        fields = ['name', 'description', 'product', 'version', 'component', 'tag']
+
+    def __init__(self, *args, **kwargs):
+        # first call parent's constructor
+        super(NewForm, self).__init__(*args, **kwargs)
+        # there's a `fields` property now
+        self.fields['product'].required = False
+        self.fields['version'].required = False
+        self.fields['component'].required = False
+        self.fields['tag'].required = False
